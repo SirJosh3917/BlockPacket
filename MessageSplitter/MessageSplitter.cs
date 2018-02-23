fe78d9e4ecb304c6a828844251bc7fd93ac96b61
SirJosh3917 <joshuaneedham@hotmail.com>
2018-02-23 07:08:45 -0500

    2x faster version

--- MessageSplitter/MessageSplitter.cs
+++ MessageSplitter/MessageSplitter.cs
@@ -5,13 +5,123 @@ using System.Text;
 
 namespace SirJosh3917 {
 	public static class MessageAlgorithms {
+		
+		/*
+		 * About twice as fast as V1
+		 * 
+		 * Takes:
+		 * 
+		 * This:
+		 *      267,082,559 ticks
+		 *      63,094 ms
+		 * 
+		 * Other:
+		 *      473,521,805 ticks
+		 *      111,862 ms
+		 * 
+		 */
+		public static string[] SimpleOverloopV2(string beginning, string custom, int max) {
+			int originalMax = max;
+
+			max = max - beginning.Length;
+
+			//ensure the message needs formatting
+
+			if (!(beginning != null && custom != null && max > 0))
+				return null;
+
+			if (0 >= max)
+				return null;
+
+			if (beginning.Length + custom.Length <= max)
+				return new string[] { beginning + custom };
+
+			//separate message into chunks with spaces
+
+			List<string> spaceChunks = new List<string>();
+			var chars = custom.ToCharArray();
+			int lastSpace = 0;
+
+			for (int i = 0; i < chars.Length; i++)
+				if (i - lastSpace > max || char.Equals(chars[i], ' ')) {
+					spaceChunks.Add(custom.Substring(lastSpace, i - lastSpace));
+					lastSpace = i;
+				}
+
+			//trim down the space chunks
+
+			for (int i = 0; i < spaceChunks.Count; i++) {
+				var trimmedMsg = spaceChunks[i].Trim();
+
+				if (trimmedMsg.Length < 1)
+					spaceChunks.RemoveAt(i--);
+				else
+					spaceChunks[i] = trimmedMsg;
+			}
+
+			//total up the message chunks into messages
+
+			List<string> messageChunks = new List<string>();
+			StringBuilder strb = new StringBuilder();
+
+			int lenTotal = 0;
+
+			for (int i = 0; i < spaceChunks.Count; i++) {
+				int thisMsgLen = spaceChunks[i].Length;
+
+				if (lenTotal + thisMsgLen > max) {
+					messageChunks.Add(strb.ToString());
+					strb.Clear();
+
+					strb.Append(spaceChunks[i]);
+					lenTotal = thisMsgLen;
+				} else {
+					strb.Append(" ");
+					strb.Append(spaceChunks[i]);
+					lenTotal += thisMsgLen;
+				}
+			}
+
+			//trim every message
+
+			for (int i = 0; i < messageChunks.Count; i++) {
+				var trimmedMsg = messageChunks[i].Trim();
+
+				if (trimmedMsg.Length < 1)
+					messageChunks.RemoveAt(i--);
+				else
+					messageChunks[i] = trimmedMsg;
+			}
+
+			//append every message to the beginning custom message
+
+			List<string> messages = new List<string>();
+
+			for (int i = 0; i < messageChunks.Count; i++)
+				messages.Add(beginning + messageChunks[i]);
+
+			//trim messages just incase
+
+			for (int i = 0; i < messages.Count; i++) {
+				var trimmedMsg = messages[i].Trim();
+
+				if (trimmedMsg.Length < 1)
+					messages.RemoveAt(i--);
+				else
+					messages[i] = trimmedMsg;
+			}
+
+			//release
+
+			return messages.ToArray();
+		}
 
 		/* 
 		 * Used for PMs:
 		 * '/pm johnathon <oh hello johnathon! this message is way over 140 charecters, but this algorithm will automatically crop this message to the'
 		 * '/pm johnathon proportional length with advanced space-before-message-ending for seamless message overloops>'
 		 */
-		public static string[] SimpleOverloop(string beginning, string custom, int max) {
+		public static string[] SimpleOverloopV1(string beginning, string custom, int max) {
 			
 			if (!(beginning != null && custom != null && max > 0))
 				return null;
